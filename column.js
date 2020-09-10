437ab9981c5b7e7c55862d790089125696aea32b
Saad Shaikh <saad@Saads-MacBook-Pro.local>
2020-09-10 15:48:53 -0400

    working on placing player token

---
+++ column.js
@@ -0,0 +1,20 @@
+export default class Column {
+    constructor(){
+        this.tokensArray = [null, null, null, null, null];
+        this.full = false;
+    }
+
+    add(playerNumber){
+        for (let i = this.tokensArray.length - 1; i >= 0; i--){
+            if (this.tokensArray[i] === null){
+                this.tokensArray[i] = playerNumber;
+            }
+        }
+    }
+
+    getTokenAt(rowIndex){
+        return this.tokensArray[rowIndex];
+    }
+
+
+}
--- game.js
+++ game.js
@@ -1,16 +1,23 @@
+import Column from './column.js';
+
 class Game {
     constructor(player1, player2){
         this.player1 = player1;
         this.player2 = player2;
         this.currentPlayer = 1;
+        this.columns = [];
+        for (let i = 0; i < 7; i++){
+            this.columns.push(new Column)
+        }
     }
+
     getName(){
         return `${this.player1} vs. ${this.player2}`;
     }
 
-    playInColumn(){
+    playInColumn(columnIndex){
+        this.columns[columnIndex].add(this.currentPlayer);
         this.currentPlayer = this.currentPlayer === 1 ? 2 : 1;
-
     }
 }
 
